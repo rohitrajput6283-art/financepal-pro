e4b1dda522a6e853fc56d30a40b6463beb73ee10
Rohit Rajput <rohitrajput6283@gmail.com>
2026-03-14 03:47:57 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,12 +1,8 @@
-'use client';
-
-// Important: These values are retrieved from environment variables.
-// Ensure you have created a .env file in the root of your project with these keys.
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || '',
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || '',
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || '',
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || '',
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '',
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || '',
+  "projectId": "studio-6164164049-1bede",
+  "appId": "1:404272890349:web:b7179d388f378b297367e4",
+  "apiKey": "AIzaSyCKW1SK83O5K6TIuItEAZuJ7-6yvo7oWaA",
+  "authDomain": "studio-6164164049-1bede.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "404272890349"
 };
